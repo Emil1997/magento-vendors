ce0f9bc034c5118eda4a4db45f721bfcfdd5d216
Emil1997 <kadilli.emil@gmail.com>
2020-08-25 22:06:03 +0200

    Database Script

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Elite\Vendors\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $table = $setup->getConnection()
+            ->newTable($setup->getTable('elite_vendors_registration'))
+            ->addColumn(
+                'id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'VENDOR ID'
+            )->addColumn(
+                'business_name',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'BUSINESS NAME'
+            )->addColumn(
+                'vat_number',
+                \Magento\Framework\DB\Ddl\Table::TYPE_NUMERIC,
+                null,
+                ['nullable' => false],
+                'VAT NUMBER'
+            )->addColumn(
+                'email_address',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'EMAIL ADDRESS'
+            )->addColumn(
+                'phone_number',
+                \Magento\Framework\DB\Ddl\Table::TYPE_NUMERIC,
+                null,
+                ['nullable' => false],
+                'PHONE NUMBER'
+            );
+        try {
+            $setup->getConnection()->createTable($table);
+        } catch (\Zend_Db_Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
