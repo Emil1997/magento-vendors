e11f8ce409ba8dca2ce9d7f029d9309745fc36cf
Emil1997 <kadilli.emil@gmail.com>
2020-08-25 23:28:00 +0200

    Get Data From General Config

---
+++ Helper/Data.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Elite\Vendors\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data
+{
+    /**
+     * @param $field
+     * @param null $storeId
+     * @return mixed
+     */
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue($field, ScopeInterface::SCOPE_STORE, $storeId);
+    }
+
+    /**
+     * @param $code
+     * @param null $storeId
+     * @return mixed
+     */
+    public function getGeneralConfig($code, $storeId = null)
+    {
+
+        return $this->getConfigValue(self::XML_PATH_HELLOWORLD .'general/'. $code, $storeId);
+    }
+}
